00a809f2c34d1f43a84623ae631f21aad3b55b35
AlYablonsky <ablonsky@austin.rr.com>
2017-02-22 13:37:38 -0600

    first commit

---
+++ wikiViewer.js
@@ -0,0 +1,48 @@
+// Comments
+// Run JQuery
+$(document).ready(function () {
+    
+    // When search is clicked run code 
+    $('#search').click(function () {
+    
+        // Gets search input 
+        var searchTerm = $('#searchTerm').val();    
+        // API url with searchTerm
+var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
+        
+        $.ajax({
+            type: "GET",
+            url: url,
+            async: false,
+            dataType: "json",
+            success: function(data){
+                
+                // Deleting the previous output
+               $('#output').html('');
+                
+                // Prepending each element from the array to html. First anchortag #3, then heading #1 and then description #2 
+                for (var i=0; i<data[1].length; i++ ) {   
+                    $('#output').prepend("<li><a href=" + data[3][i] + ">" + data[1][i] + "</a><p>" + data[2][i] + "</p></li>");
+                } 
+                // Deletes the search term in the form upon initiating the search
+                $('#searchTerm').val('');
+            },
+            error: function(errorMessage){
+                alert("Error");
+            }
+            
+        });  // Corresponds to ajax commad
+    
+       
+    });  // Corresponds to click (search )
+    
+    // Performs submission through the "enter" key.
+     $("#searchTerm").keypress(function(e) {
+            if(e.which===13){
+                $("#search").click();
+            }    
+        })
+    
+        
+});  // corresponds to document ready
+    
